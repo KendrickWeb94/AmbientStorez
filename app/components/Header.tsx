1b7c1eaac32f62cb84c3215e4caf16832b983d00
Kendrickweb94 <sunflowerroses801@gmail.com>
2024-06-27 13:42:49 +0100

    hero completed

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -222,11 +222,11 @@ export const Navbar = () => {
                     onClick={() => setShowDropdown(!showDropdown)}
                   />
                   {showDropdown && (
-                    <div className="absolute translate-y-8 p-2 right-0 mt-12 w-36 bg-white border-gray-400/45 border rounded-lg shadow-lg z-10">
+                    <div className="absolute translate-y-16 p-2 right-0 mt-12 w-36 bg-white dark:bg-black  border-gray-400/45 border rounded-lg shadow-lg z-10">
                       <ul className=" flex-col-reverse flex">
                         <li
                           onClick={handleSignOut}
-                          className="p-2 cursor-pointer text-sm flex items-center gap-3 rounded hover:bg-gray-200"
+                          className="p-2 cursor-pointer text-sm flex items-center gap-3 rounded hover:bg-gray-300 dark:hover:bg-zinc-800"
                         >
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -246,7 +246,7 @@ export const Navbar = () => {
                           <p>Sign Out</p>
                         </li>
                         <hr></hr>
-                        <li className="p-2 flex rounded items-center gap-3 cursor-pointer hover:bg-gray-200">
+                        <li className="p-2 flex rounded items-center gap-3 cursor-pointer hover:bg-gray-300 dark:hover:bg-zinc-800">
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
@@ -264,6 +264,46 @@ export const Navbar = () => {
 
                           <Link className="text-sm" href="#/profile">
                             Profile
+                          </Link>
+                        </li>
+                        <li className="p-2 flex rounded items-center gap-3 cursor-pointer hover:bg-gray-300 dark:hover:bg-zinc-800">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="size-4"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z"
+                            />
+                          </svg>
+
+                          <Link className="text-sm" href="./Vendor">
+                            vendor
+                          </Link>
+                        </li>
+                        <li className="p-2 flex rounded items-center gap-3 cursor-pointer hover:bg-gray-300 dark:hover:bg-zinc-800">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="size-4"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                            />
+                          </svg>
+
+                          <Link className="text-sm" href="./Customer">
+                            Customer
                           </Link>
                         </li>
                       </ul>
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,16 +1,87 @@
-import React from "react"
+import React from "react";
+import Image from "next/image";
+import heroimg from "../assets/svg-icons/hero-img.png";
+import iphonesvg from "../assets/svg-icons/iphone-heri.svg";
 
- 
- export const Hero = () => {
-        return(
-            <div className="w-full">
-                <div className="w-full flex mx-auto max-w-[94%]">
-                    <div className="w-[217px] h-80 border-r border-r-gray-500/45 p-2">
-                        <ul className=" flex space-y-3">
-                            
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        )
- }
+export const Hero = () => {
+  return (
+    <div className="w-full">
+      <div className="w-full gap-24 flex mx-auto max-w-[94%]">
+        <div className="w-[217px] pt-10 h-auto border-r border-r-gray-500/45 p-3">
+          <ul className="cursor-pointer flex space-y-5 flex-col">
+            <li className="text-sm font-medium flex items-center gap-3">
+              {" "}
+              <p>Women's Fashion</p>{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
+            </li>
+            <li className="text-sm font-medium flex items-center gap-3">
+              {" "}
+              <p>Men's Fashion</p>{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                />
+              </svg>
+            </li>
+            <li className="text-sm font-medium flex items-center gap-3">
+              {" "}
+              <p>Electronics</p>{" "}
+            </li>
+            <li className="text-sm font-medium flex items-center gap-3">
+              {" "}
+              <p>Home & Lifestyle</p>{" "}
+            </li>
+            <li className="text-sm font-medium flex items-center gap-3">
+              {" "}
+              <p>Medicine</p>{" "}
+            </li>
+            <li className="text-sm font-medium flex items-center gap-3">
+              {" "}
+              <p>Sports & Outdoors</p>{" "}
+            </li>
+            <li className="text-sm font-medium flex items-center gap-3">
+              {" "}
+              <p>Babies & Toys</p>{" "}
+            </li>
+            <li className="text-sm font-medium flex items-center gap-3">
+              {" "}
+              <p>Groceries & Pets</p>{" "}
+            </li>
+            <li className="text-sm font-medium flex items-center gap-3">
+              {" "}
+              <p>Health & Beauty</p>{" "}
+            </li>
+          </ul>
+        </div>
+
+        <div className="p-3 pt-10">
+          <div className=" flex-grow flex cursor-pointer items-center overflow-hidden rounded-md h-[375] bg-black p-3">
+            <Image src={iphonesvg} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
